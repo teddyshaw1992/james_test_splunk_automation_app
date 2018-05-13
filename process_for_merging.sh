c952cc940b1b0deff1504a1b12a05df8c4cd7bc8
James Shaw <j.a.shaw@accenture.com>
2018-05-14 09:23:06 +1000

    updates to post-merge and process_for_merging.sh

--- process_for_merging.sh
+++ process_for_merging.sh
@@ -11,9 +11,15 @@ git checkout test
 
 # Get all the merges since the last tag
 #git log `git describe --tags --abbrev=0`..HEAD --oneline
-KINGSHIT=git log --merges `git describe --tags --abbrev=0`..HEAD --oneline
+KINGSHIT=$( git log --merges `git describe --tags --abbrev=0`..HEAD --oneline )
 
 
 while read -r KINGSHIT; do
-  echo "... ${KINGSHIT} ..."
+  #echo "... ${KINGSHIT} ..."
+  FROM_BRANCH=$( expr "fb4dc0d Merge branch 'dev' into test" : '.* Merge branch \(.*\) into .*' )
+  echo "${FROM_BRANCH}"
+
+  TO_BRANCH=$( expr "fb4dc0d Merge branch 'dev' into test" : '.* Merge branch .* into \(.*\)' )
+  echo "${TO_BRANCH}"
+
 done <<< "${KINGSHIT}"
