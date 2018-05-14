c4c64d6e06ee8e39ae6624544dd07bc1fae80b37
James Shaw <j.a.shaw@accenture.com>
2018-05-14 22:30:01 +1000

    CRYS-10 Updated list_git_merges.sh

--- list_git_merges.sh
+++ list_git_merges.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+#git add *
+#git commit -m "Whatever you want"
+#git push
 
-LIST_OF_JIRA_IDS=()
+#git checkout test
+
+# Enforce using the default merge commit message
+#git merge dev --no-edit --no-ff
+
+# Get all the merges since the last tag
+#git log `git describe --tags --abbrev=0`..HEAD --oneline
+MERGE_COMMIT_INFO=$( git log --merges `git describe --tags --abbrev=0`..HEAD --oneline )
 
 
+while read -r MERGE_COMMIT_INFO; do
+  #echo "... ${KINGSHIT} ..."
+  FROM_BRANCH=$( expr "${MERGE_COMMIT_INFO}" : '.* Merge branch \(.*\) into .*' )
+  echo "${FROM_BRANCH}"
 
-LIST_OF_JIRA_IDS+=('${JIRA_ID}')
+  TO_BRANCH=$( expr "${MERGE_COMMIT_INFO}" : '.* Merge branch .* into \(.*\)' )
+  echo "${TO_BRANCH}"
+
+done <<< "${MERGE_COMMIT_INFO}"
